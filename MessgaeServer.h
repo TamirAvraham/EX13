58092d98213498a3a91b8651dbc5a16fd33379bf
tamir avraham <tamir.avrah@gmail.com>
2023-02-10 17:55:43 +0200

    i switched computers so i needed to commit to switch

--- MessgaeServer.h
+++ MessgaeServer.h
@@ -5,21 +5,26 @@
 #include <string>
 #include <functional>
 #include <queue>
-#include <list>
+#include <vector>
+#include <map>
+#include <fstream>
 class MessgaeServer:public Server
 {
 	using user = std::pair<std::string, SOCKET>;
+	using convo = std::pair<std::string, std::fstream>;
 public:
 	MessgaeServer();
 	void init();
 private:
-	std::list<user> _users;
+	std::vector<user> _users;
+	std::map< std::string, std::fstream > _convos;
 	std::queue<std::string> _msgs;
 
 	void clientHandler(SOCKET) override;
 	void sendAcceptClient(SOCKET)const;
 
 	std::string getConnectedUsers();
+	
 
 };
 
